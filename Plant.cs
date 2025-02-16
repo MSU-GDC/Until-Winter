f9bef94c42b7fd9c869a5425a4c5dcbbecad662e
Times-End <lknudson05@outlook.com>
2025-02-15 18:25:12 -0700

    add base plant and tile files

---
+++ Plant.cs
@@ -0,0 +1,10 @@
+public class Plant {
+
+    private String name;
+    private int value;
+    private int[] season;
+    private int growTime;
+    private int waterRate;
+    private int harvestRate;
+
+}
