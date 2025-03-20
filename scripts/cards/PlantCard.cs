1db49ff4c631cd93e85545ee501cb5f4ec066fe2
John F <johnjamisonfoth@gmail.com>
2025-03-20 14:13:27 -0600

    NEW GODOT TECH JUST DROPPED (Resources). Also working on implimenting plant card selection and usage mechanics.

--- scripts/cards/PlantCard.cs
+++ scripts/cards/PlantCard.cs
@@ -1,13 +1,17 @@
 using Godot;
 using System;
 
-public partial class PlantCard: ICard
+public partial class PlantCard: Resource, ICard
 {
-    private string _name; 
-    private Plant _plant; 
-    private string _description;
 
-    private CardID _id;
+    public static readonly PlantCard PLACEHOLDER = new PlantCard("Placeholder","This is a placeholder.",new Plant("Placeholder Plant",0,(int)SeasonFlags.NONE,0,0,0),CardID.PL_GENERICPLANT);
+    [ExportGroup("Fields")]
+    
+    [Export] private string _name; 
+    [Export] private Plant _plant; 
+    [Export] private string _description;
+
+    [Export] private CardID _id;
 
 
     public PlantCard(string name, string description, Plant plant, CardID id){
